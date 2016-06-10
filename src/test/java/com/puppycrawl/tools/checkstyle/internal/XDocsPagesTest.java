73941d5234725e2121c63dcff789925f3d9d1900
rnveach <rveach02@gmail.com>
2016-06-10 07:55:43 -0400

    minor: start

--- src/test/java/com/puppycrawl/tools/checkstyle/internal/XDocsPagesTest.java
+++ src/test/java/com/puppycrawl/tools/checkstyle/internal/XDocsPagesTest.java
@@ -149,6 +149,33 @@ public class XDocsPagesTest {
     }
 
     @Test
+    public void testChecksPage() throws Exception {
+        final String fileName = AVAILABLE_CHECKS_PATH.toString();
+        final String input = new String(Files.readAllBytes(AVAILABLE_CHECKS_PATH), UTF_8);
+        final Document document = XmlUtil.getRawXml(fileName, input, input);
+        final NodeList rows = document.getElementsByTagName("tr");
+        String lastCheckName = null;
+
+        for (int position = 0; position < rows.getLength(); position++) {
+            final Node row = rows.item(position);
+            final List<Node> columns = new ArrayList<>(XmlUtil.findChildElementsByTag(row, "td"));
+            final String checkName = columns.get(0).getTextContent();
+
+            Assert.assertTrue(fileName + " '" + checkName + "' shouldn't end with 'Check'",
+                    !checkName.endsWith("Check"));
+            if (lastCheckName != null) {
+                Assert.assertTrue(
+                        fileName + " section '" + checkName + "' is out of order compared to '"
+                                + lastCheckName + "'",
+                        checkName.toLowerCase(Locale.ENGLISH).compareTo(
+                                lastCheckName.toLowerCase(Locale.ENGLISH)) >= 0);
+            }
+
+            lastCheckName = checkName;
+        }
+    }
+
+    @Test
     public void testAllXmlExamples() throws Exception {
         for (Path path : XDocUtil.getXdocsFilePaths()) {
             final String input = new String(Files.readAllBytes(path), UTF_8);
